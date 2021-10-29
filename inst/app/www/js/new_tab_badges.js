50199eaa5f12896fd82f16f96b4a35a9dd8c8a7c
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-10-29 15:31:23 -0400

    New badge to bed unit utilization

--- inst/app/www/js/new_tab_badges.js
+++ inst/app/www/js/new_tab_badges.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  var new_els = ['dq_provider_level','client_counts']
+  var new_els = ['dq_provider_level','client_counts', 'utilization']
   new_els.map(function(id) {
     var el = document.getElementById("tab-" + id)
     el.innerHTML += '&nbsp<span class="badge badge-success">New</span>'
