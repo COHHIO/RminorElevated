aec1b30453de0ba01d83621bf7269831a1088983
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-11-09 21:00:29 -0500

    Add badge to prioritization tabl

--- inst/app/www/js/new_tab_badges.js
+++ inst/app/www/js/new_tab_badges.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  var new_els = ['dq_provider_level','client_counts', 'utilization']
+  var new_els = ['dq_provider_level','client_counts', 'utilization', 'prioritization']
   new_els.map(function(id) {
     var el = document.getElementById("tab-" + id)
     el.innerHTML += '&nbsp<span class="badge badge-success">New</span>'
