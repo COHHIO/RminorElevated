2720b2764a722d308f12461c4b0022ff1d46ac96
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-06 17:33:53 -0500

    _dq_system_wide_: add timeliness median plot

--- R/mod_body_dq_system_wide.R
+++ R/mod_body_dq_system_wide.R
@@ -20,7 +20,22 @@ mod_body_dq_system_wide_ui <- function(id){
 mod_body_dq_system_wide_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+    output$desk_time_medians <- renderPlot({
+      ggplot(
+        head(desk_time_medians, 10L),
+        aes(
+          x = reorder(ProjectName, MedianDeskTime),
+          y = MedianDeskTime,
+          fill = MedianDeskTime
+        )
+      ) +@
+        geom_col(show.legend = FALSE) +
+        coord_flip() +
+        labs(x = "",
+             y = "Median Days") +
+        scale_fill_viridis_c(direction = -1) +
+        theme_minimal(base_size = 18)
+    })
   })
 }
     
