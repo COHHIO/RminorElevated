6cf0300fc710848c5a03a19c3c887f615595afb1
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-06 19:11:28 -0500

    _sidebar_ : deprecate unsheltered

--- R/mod_sidebar.R
+++ R/mod_sidebar.R
@@ -64,10 +64,11 @@ mod_sidebar_ui <- function(id){
             text = "Data Entry Timeliness",
             tabName = "dq_timeliness" #deskTime
           ),
-          bs4Dash::bs4SidebarMenuSubItem(
-            text = "Unsheltered",
-            tabName = "dq_unsheltered"
-          ),
+          # Deprecated in Clarity
+          # bs4Dash::bs4SidebarMenuSubItem(
+          #   text = "Unsheltered",
+          #   tabName = "dq_unsheltered" #dqUnsh
+          # ),
           bs4Dash::bs4SidebarMenuSubItem(
             text = "Region-level",
             tabName = "dq_region_level" #dqRegion
