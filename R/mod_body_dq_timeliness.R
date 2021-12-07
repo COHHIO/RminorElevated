2ac55b444846191c8ffd532a97c63885bb11cbdb
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-06 19:11:06 -0500

    dq_timeliness: remove plotly from output

--- R/mod_body_dq_timeliness.R
+++ R/mod_body_dq_timeliness.R
@@ -12,7 +12,7 @@ mod_body_dq_timeliness_ui <- function(id){
   shiny::tagList(
     ui_header_row(),
     ui_picker_project(choices = desk_time_providers),
-    ui_row_box(plotly::plotlyOutput(ns("detail")),
+    ui_row_box(shiny::plotOutput(ns("detail")),
                                title = "Detail"),
     ui_row_box(title = "Guidance", shiny::HTML("
       <h4>HUD and Data Quality</h4>
@@ -28,7 +28,7 @@ mod_body_dq_timeliness_ui <- function(id){
         <h4>How Do We Fix This?</h4>
         <p><strong>There is nothing a user can do to 'correct' a client entered into the system outside the window, we can only resolve to enter clients within the 5-day range going forward.</strong> As you catch up on data entry, you may see the median delay gets longer at first, but this data looks back exactly one year, so any clients with an Entry Date over a year ago will fall off of this plot and your median will change accordingly.</p>
         <h4>Interpretation</h4>
-        <p><span style='color:forestgreen;'>Green</span> dots here represent clients entered within the range and <span style='color:orangered;'>orange</span> dots represent clients entered outside the range. The darker the dot, the more clients entered your project on that day. (Likely a household.)</p>
+        <p><span style='color:forestgreen;'>Green</span> dots here represent clients entered within the range and <span style='color:orangered;'>orange</span> lines (spanning the Entry Date to the Date the record was created) represent clients entered outside the range. The darker the dot, the more clients entered your project on that day. (Likely a household.)</p>
         <p>The metric COHHIO looks at here is the Median, so if you have orange dots but your Median is within the 5 day range, that is great!</p>
         <p>If you have orange dots BELOW the 0 mark, that means you entered Entry Dates into the future, which means there is potentially a mis-keyed date or the user needs technical assistance about how to know what date to enter for the Entry Date. If this is the case, please <a href='mailto:hmis@cohhio.org' target='_blank'>email the HMIS team at hmis@cohhio.org</a></p>.
       <h4>Is it possible there's a mistake?</h4>
@@ -46,8 +46,7 @@ mod_body_dq_timeliness_server <- function(id){
     
     ReportStart <- Sys.Date() - lubridate::years(1)
     ReportEnd <- Sys.Date()
-    output$header <- shiny::renderUI(RminorElevated::server_header("Data Entry Timeliness",
-                                                                   x = shiny::h3(paste0("Fixed Date Range: ", ReportStart, " to ", ReportEnd))))
+    output$header <- shiny::renderUI(RminorElevated::server_header("Data Entry Timeliness", shiny::h3(paste0("Fixed Date Range: ", ReportStart, " to ", ReportEnd))))
     
     
     project <- shiny::eventReactive(input$project, {input$project}) |> shiny::debounce(1000)
