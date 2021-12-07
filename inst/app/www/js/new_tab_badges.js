579ceb622ccfb516e3e5287cb0ff72df3b9af878
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-06 19:12:10 -0500

    All docs & js

--- inst/app/www/js/new_tab_badges.js
+++ inst/app/www/js/new_tab_badges.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  var new_els = ['dq_provider_level', 'dq_timeliness','client_counts', 'utilization', 'prioritization']
+  var new_els = ['dq_provider_level', 'dq_timeliness', 'dq_region_level','client_counts', 'utilization', 'prioritization']
   new_els.map(function(id) {
     var el = document.getElementById("tab-" + id)
     el.innerHTML += '&nbsp<span class="badge badge-success">New</span>'
