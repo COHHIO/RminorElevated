2faf224aaca312f2636cfab6ece85c806ce88182
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-10 07:24:11 -0500

    Add better guidance, deprecate missing client location

--- R/mod_body_dq_provider_level.R
+++ R/mod_body_dq_provider_level.R
@@ -70,7 +70,7 @@ mod_body_dq_provider_level_server <- function(id){
     output$header <- renderUI({
       server_header(title = "Data Quality", date_range = input$date_range)
     })
-    
+    guidance <- guidance()
     dq_profiles <- dq_main()
     
     eligibility_detail <- eligibility_detail()
@@ -79,19 +79,23 @@ mod_body_dq_provider_level_server <- function(id){
     # TODO Should be a descriptionBox, and go in a section with others.
     output$dq_APsNoReferrals <- renderUI({
       req(project())
-      AP_not_doing_referrals <- aps_no_referrals()  |> 
-        dplyr::filter(ReferringProjectID %in% project())
+      AP_no_referrals <- aps_no_referrals()  |> 
+        dplyr::filter(ProjectID %in% project())
       
-      if (nrow(AP_not_doing_referrals) > 0) {
-        ui_row_box(
+      if (nrow(AP_no_referrals) > 0) {
+        ui_solid_box(
           id = "noreferrals",
           title = "Access Point Has No Outgoing Referrals",
           status = "danger",
           width = 12,
-          shiny::tags$p(
-            "Access Points should be creating Referrals in HMIS so that households can be connected to housing. Please 
-<a href=\"http://hmis.cohhio.org/index.php?pg=kb.page&id=186\">click here</a> for more information."
+          shiny::tagList(
+            # TODO print names of APS
+            shiny::tags$p(
+              "The following Access Points ",
+              do.call(shiny::tags$ol, purrr::map(AP_no_referrals$ProjectName, ~shiny::tags$li(.x))),
+              " should be creating Referrals in HMIS so that households can be connected to housing. Please ", shiny::tags$a(href = "http://hmis.cohhio.org/index.php?pg=kb.page&id=186", target = "_blank", "click here"), "for more information.")
           )
+          
         )
       } else {
         
@@ -121,7 +125,7 @@ mod_body_dq_provider_level_server <- function(id){
           title = "Household Issues",
           status = "warning",
           shiny::tags$p(
-            "Please correct your Household Issues before moving on to make other Data Quality corrections."
+            "Please correct Household Issues before moving on to make other Data Quality corrections.", dq_see_guidance()
           ),
           datatable_default(HHIssues, escape = FALSE)
         )
@@ -145,7 +149,7 @@ mod_body_dq_provider_level_server <- function(id){
           title = "Duplicate Entry Exits",
           status = "warning",
           shiny::tags$p(
-            "Please correct this issue before moving on to your other errors.<br> Duplicate Entry Exits are created when the user clicks \"Add Entry Exit\" instead of clicking the Entry pencil to get back into an assessment. These must be deleted for each member of the household. Please take care to not delete Entry Exits with valid Interims attached."
+            guidance$duplicate_ees
           ),
           datatable_default(DuplicateEEs, escape = FALSE)
         )
@@ -156,29 +160,29 @@ mod_body_dq_provider_level_server <- function(id){
     })
     
 
-    
-    output$dq_MissingLocation <- renderUI({
-      req(project())
-      HHIssues <- dq_profiles |> 
-        dq_filter_between(date_range = input$date_range, project = project(), Issue == "Missing Client Location") |> 
-        dq_select_cols()
-      
-      
-      if (nrow(HHIssues)) {
-        ui_solid_box(
-          id = "location",
-          title = "Missing Client Location",
-          status = "warning",
-          shiny::tags$p(
-            "Households with a missing Client Location (the data element just after the Relationship to Head of Household) will be completely excluded from ALL HUD reporting."
-          ),
-          datatable_default(HHIssues, escape = FALSE)
-        )
-      }
-      else {
-        
-      }
-    })
+    # Deprecated in Clarity
+    # output$dq_MissingLocation <- renderUI({
+    #   req(project())
+    #   HHIssues <- dq_profiles |> 
+    #     dq_filter_between(date_range = input$date_range, project = project(), Issue == "Missing Client Location") |> 
+    #     dq_select_cols()
+    #   
+    #   
+    #   if (nrow(HHIssues)) {
+    #     ui_solid_box(
+    #       id = "location",
+    #       title = "Missing Client Location",
+    #       status = "warning",
+    #       shiny::tags$p(
+    #         guidance$missing_client_loc
+    #       ),
+    #       datatable_default(HHIssues, escape = FALSE)
+    #     )
+    #   }
+    #   else {
+    #     
+    #   }
+    # })
     
     output$dq_PATHMissingContact <- renderUI({
       req(project())
@@ -192,10 +196,7 @@ mod_body_dq_provider_level_server <- function(id){
           title = "Missing Contact (PATH)",
           status = "warning",
           datatable_default(MissingPATHContact, escape = FALSE),
-          tagList(MissingPathContact |> 
-            dplyr::select(Guidance) |> 
-            unique() |> 
-              shiny::tags$p())
+          tags$p(guidance$missing_path_contact)
         )
       }
       else {
@@ -227,7 +228,7 @@ mod_body_dq_provider_level_server <- function(id){
           solidHeader = TRUE,
           width = 12,
           shiny::tags$p(
-            "Your Residence Prior data suggests that this project is either serving ineligible households, the household was entered into the wrong project, or the Residence Prior data at Entry is incorrect. Please check the terms of your grant or speak with the CoC team at COHHIO if you are unsure of eligibility criteria for your project type."
+            guidance$check_eligibility
           ),
           datatable_default(Ineligible, escape = FALSE)
         )
@@ -243,22 +244,25 @@ mod_body_dq_provider_level_server <- function(id){
     output$dq_OverlappingEEs <- renderUI({
       req(project())
       OverlappingEEs <- dq_overlaps() |>
-        dq_filter_between(date_range = input$date_range, project = project(), Issue == "Overlapping Project Stays") |>
+        dq_filter_between(date_range = input$date_range, project = project()) |>
         dq_select_cols(
+          "Entry Date" = EntryDate,
           "Exit Date" = ExitDate,
-          "Move In Date" = MoveInDateAdjust,
-          "Overlaps With This Provider's Stay" = PreviousProject
+          "Move-In Date" = MoveInDateAdjust,
+          "Overlaps",
+          "Issue",
+          "Guidance",
+          default = NULL
         ) 
         
       if (nrow(OverlappingEEs)) {
         ui_solid_box(
           id = "overlappers",
-          title = "Overlapping Entry Exits",
+          title = "Overlapping Project Stays",
           status = "warning",
           width = 12,
-          shiny::tags$p(
-            "A client cannot reside in an ES, TH, or Safe Haven at the same time. Nor can they have a Move-In Date into a PSH or RRH project while they are still in an ES, TH, or Safe Haven. Further, they cannot be in any two RRH's or any two PSH's simultaneously, housed or not.<br> Please look the client(s) up in HMIS and determine which project stay's Entry/Move-In/or Exit Date is incorrect. PLEASE NOTE: It may be the \"Previous Provider's\" mistake, but if you are seeing clients here, it means your project stay was entered last. <br> If the overlap is not your project's mistake, please work with the project that has the incorrect Entry/Move-In/or Exit Date to get this corrected  or send an email to hmis@cohhio.org if you cannot get it resolved. These clients will NOT show on their Data Quality app. <br> If YOUR dates are definitely correct, it is fine to continue with other data corrections as needed."),
-          datatable_default(OverlappingEEs, escape = FALSE)
+          datatable_default(OverlappingEEs, escape = FALSE),
+          shiny::tags$p(guidance$project_stays)
         )
       } else {
         
@@ -281,7 +285,7 @@ mod_body_dq_provider_level_server <- function(id){
             Type == "Error"
         ) |>
         dq_select_cols() |> 
-        datatable_default(escape = FALSE)
+        datatable_default(escape = FALSE),
     })
     
     output$dq_Warnings <- DT::renderDataTable({
