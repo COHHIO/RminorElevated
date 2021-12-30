24f5133f8ff88842ab85a46d19e85df5a9bb72b8
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-29 20:50:33 -0500

    app title change

--- R/app_ui.R
+++ R/app_ui.R
@@ -36,7 +36,7 @@ golem_add_external_resources <- function(){
     favicon(),
     bundle_resources(
       path = app_sys('app/www'),
-      app_title = 'RminorElevated'
+      app_title = 'R minor Elevated'
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert() 
