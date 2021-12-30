7eeeb0868b4641a06b459de58c3c7d68cfd26670
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-29 21:00:13 -0500

    Add sum to RRH/HP Spending

--- R/qpr_expr_rrh_spending.R
+++ R/qpr_expr_rrh_spending.R
@@ -9,7 +9,6 @@ spending_filter <- function(x, date_range, region, ProjectType) {
 }
 qpr_expr$rrh_spending$expr <- rlang::expr({
   .data <- qpr_spending()
-  browser()
   list(
     rrh = spending_filter(.data, input$date_range, input$region, 13),
     hp = spending_filter(.data, input$date_range, input$region, 12)
@@ -19,12 +18,14 @@ qpr_expr$rrh_spending$expr <- rlang::expr({
 spending_dt <- function(x) {
   x |> 
     dplyr::mutate(ProjectName = as.factor(ProjectName)) |>
+    dplyr::group_by(UniqueID, ProjectName, ServiceStartDate, ServiceItemName) |> 
+    dplyr::summarize(Amount = sum(ServiceAmount)) |> 
     dplyr::select(
       UniqueID,
       "Project Name" = ProjectName,
       "Service Date" = ServiceStartDate,
       Description = ServiceItemName,
-      Amount = ServiceAmount
+      Amount
     ) |> 
     datatable_default(escape = FALSE)
 }
