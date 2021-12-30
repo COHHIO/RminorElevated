b6268bf37aff14b386aa1ead9fdbe312254e71d0
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-29 20:56:38 -0500

    new badges

--- inst/js/new_tab_badges.js
+++ inst/js/new_tab_badges.js
@@ -1,11 +1,10 @@
-$( '#sidebar' ).ready(function() {
-  var new_els = ['dq_provider_level', 'dq_timeliness', 'dq_region_level','client_counts', 'utilization', 'prioritization', 'vet_active_list', 'dq_system_summary']
+$( '.main-sidebar' ).ready(function() {
+  var new_els = ['dq_provider_level', 'dq_timeliness', 'dq_region_level','client_counts', 'utilization', 'prioritization', 'vet_active_list', 'dq_system_summary', "qpr_community_need_ph", "qpr_community_need_lh", "qpr_length_of_stay"]
   new_els.map(function(id) {
     var checkExist = setInterval(function() {
    if ($('#'+"tab-"+ id).length) {
-      var el = document.getElementById("tab-" + id)
-    el.innerHTML += '&nbsp<span class="badge badge-success" style="float:right;">New</span>'
-    el = el.setAttribute("style", "display: inline-flex;align-items: center;")
+      var el = $("#tab-" + id).append('<span class="badge badge-success" style="float:right;">New</span>')
+    
       clearInterval(checkExist);
    }
 }, 100); 
