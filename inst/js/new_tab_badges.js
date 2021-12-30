2fa316faa0fc97693eda44fcecfa0bb4268ccbee
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-30 18:59:38 -0500

    green icons instead of badges

--- inst/js/new_tab_badges.js
+++ inst/js/new_tab_badges.js
@@ -1,10 +1,12 @@
 $( '.main-sidebar' ).ready(function() {
-  var new_els = ['dq_provider_level', 'dq_timeliness', 'dq_region_level','client_counts', 'utilization', 'prioritization', 'vet_active_list', 'dq_system_summary', "qpr_community_need_ph", "qpr_community_need_lh", "qpr_length_of_stay"]
+  var new_els = ['welcome', 'dq_provider_level', 'dq_timeliness', 'dq_region_level','client_counts', 'utilization', 'prioritization', 'vet_active_list', 'dq_system_summary', "qpr_community_need_ph", "qpr_community_need_lh", "qpr_length_of_stay", "qpr_noncash_benefits", "qpr_permanent_housing", "qpr_income_growth", "qpr_health_insurance", "qpr_rrh_placement", "qpr_rrh_spending"]
   new_els.map(function(id) {
     var checkExist = setInterval(function() {
    if ($('#'+"tab-"+ id).length) {
-      var el = $("#tab-" + id).append('<span class="badge badge-success" style="float:right;">New</span>')
-    
+      var el = $("#tab-" + id)
+      el.children("i.fa").addClass('text-success')
+      if (id.match('^qpr|^dq'))
+        var a = el.parents(".nav-item.has-treeview").find("i.fa").first().addClass('text-success')
       clearInterval(checkExist);
    }
 }, 100); 
