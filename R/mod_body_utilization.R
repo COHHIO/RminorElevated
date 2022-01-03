0607c8abfbcc0b99ff06c31f48c02cf105ad4870
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2022-01-03 17:41:43 -0500

    utilization: Remove multi select as it complicates the math

--- R/mod_body_utilization.R
+++ R/mod_body_utilization.R
@@ -22,7 +22,8 @@ mod_body_utilization_ui <- function(id) {
   shiny::fluidPage(
     ui_header_row(),
     ui_row(
-      ui_picker_project(choices = choices),
+      ui_picker_project(choices = choices,
+                        multiple = FALSE),
       shinyWidgets::airDatepickerInput(
         inputId = ns("date_range"),
         label = "Click to Choose a Month",
@@ -70,7 +71,7 @@ mod_body_utilization_server <- function(id){
   shiny::moduleServer( id, function(input, output, session){
     ns <- session$ns
     
-    output$header <- shiny::renderUI(server_header("Bed and Unit Utilization", input$project, format(input$date_range, "%B %Y"), shiny::tags$p(shiny::icon("exclamation-triangle", style = "display:inline-block; color: #ffc107;"), "During this time, congregate facilities should be aiming to deconcentrate. If this causes fluctuations in Utilization, that is okay. Please continue to keep your clients safe.", status = "warning")))
+    output$header <- shiny::renderUI(server_header("Bed and Unit Utilization", format(input$date_range, "%B %Y"), shiny::tags$p(shiny::icon("exclamation-triangle", style = "display:inline-block; color: #ffc107;"), "During this time, congregate facilities should be aiming to deconcentrate. If this causes fluctuations in Utilization, that is okay. Please continue to keep your clients safe.", status = "warning")))
     
     ReportStart <- eventReactive(input$date_range,{
       lubridate::floor_date(input$date_range,
@@ -81,19 +82,21 @@ mod_body_utilization_server <- function(id){
       ReportStart() |>
         format(format = "%b%Y")
     })
-    
+    server_debounce(input$project)
     uc_selected <- reactive({
+      req(project(), ReportStart(), ReportEnd(), input$project)
       utilization_clients() |>
         HMIS::served_between(ReportStart(), ReportEnd()) |> 
-        dplyr::filter(ProjectID %in% input$project) |>
+        dplyr::filter(ProjectID %in% project()) |>
         dplyr::mutate(BedStart = dplyr::if_else(ProjectType %in% c(3, 9, 13),
                                                 MoveInDateAdjust, EntryDate)) |>
         dplyr::select(UniqueID, BedStart, ExitDate, dplyr::all_of(col_nm()))
     })
     bed_count <- reactive({
+      req(project(), ReportStart(), ReportEnd())
        utilization_beds() |> 
         HMIS::beds_available_between(ReportStart(), ReportEnd()) |> 
-        dplyr::filter(ProjectID %in% input$project) |>
+        dplyr::filter(ProjectID %in% project()) |>
         dplyr::group_by(ProjectID) |>
         dplyr::summarise(BedCount = sum(BedInventory), .groups = "drop") |>
         dplyr::pull(BedCount)
@@ -103,6 +106,7 @@ mod_body_utilization_server <- function(id){
     
     output$detail <-
       DT::renderDT(server = FALSE, {
+        req(input$project, uc_selected())
         uc_selected() |> 
           rlang::set_names(c("Unique ID", "Bed Start", "Exit Date", paste("Bed Nights in", format(ReportStart(), "%B %Y")))) |> 
           datatable_default(escape = FALSE)
@@ -110,6 +114,7 @@ mod_body_utilization_server <- function(id){
     
     output$infobox_bn_served <-
       bs4Dash::renderInfoBox({
+        req(input$project, uc_selected())
         bs4Dash::infoBox(
           title = "Total Bed Nights Served",
           color = "purple",
@@ -122,28 +127,30 @@ mod_body_utilization_server <- function(id){
     
     output$infobox_pbn_available <-
       bs4Dash::renderInfoBox({
-        
+        req(input$project, bed_count(), daysInMonth())
         bs4Dash::infoBox(
           title = "Possible Bed Nights",
           color = "purple",
           icon = shiny::icon("bed"),
-          value = bed_count() * daysInMonth(),
+          value = sum(bed_count() * daysInMonth()),
           subtitle = paste(
             "Bed Count:",
-            bed_count(),
+            sum(bed_count()),
             "beds x",
-            daysInMonth(),
+            sum(daysInMonth()),
             "days in",
             format(ReportStart(), "%B"),
             "=",
-            bed_count() * daysInMonth()
+            sum(bed_count() * daysInMonth())
           )
         )
       })
     
     output$infobox_utilization <-
       bs4Dash::renderInfoBox({
-        bedUtilization <- scales::percent(sum(uc_selected()[[col_nm()]]) / (bed_count() * daysInMonth()))
+        req(input$project, uc_selected(), bed_count(), daysInMonth())
+        
+        bedUtilization <- scales::percent(sum(uc_selected()[[col_nm()]]) / sum(bed_count() * daysInMonth()))
         bs4Dash::infoBox(
           title = "Bed Utilization",
           color = "teal",
@@ -151,7 +158,7 @@ mod_body_utilization_server <- function(id){
           value = bedUtilization,
           subtitle = paste(sum(uc_selected()[[col_nm()]]),
                            "/",
-                           bed_count() * daysInMonth(),
+                           sum(bed_count() * daysInMonth()),
                            "=",
                            bedUtilization)
         )
