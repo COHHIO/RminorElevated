db005cf407dbe7ed58aeed681614b445ea31e451
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2022-01-28 09:30:51 -0500

    Fix trailing period.

--- R/mod_body_dq_timeliness.R
+++ R/mod_body_dq_timeliness.R
@@ -24,7 +24,9 @@ dt_time <- validation() |>
                 GoalMet) 
 
 desk_time_providers <- rlang::set_names(dt_time$ProjectID |> unique(), dt_time$ProjectName |> unique())
-
+# DEBUG
+# input <- list(date_range = c(lubridate::ymd("2020-01-01"), lubridate::ymd("2022-01-21")),
+#               program = "558")
 #' body_dq_timeliness UI Function
 #'
 #' @description A shiny Module.
@@ -60,7 +62,7 @@ mod_body_dq_timeliness_ui <- function(id){
         <h4>Interpretation</h4>
         <p><span style='color:forestgreen;'>Green</span> dots here represent clients entered within the range and <span style='color:orangered;'>orange</span> lines (spanning the Entry Date to the Date the record was created) represent clients entered outside the range. The darker the dot, the more clients entered your program on that day. (Likely a household.)</p>
         <p>The metric COHHIO looks at here is the Median, so if you have orange dots but your Median is within the 5 day range, that is great!</p>
-        <p>If you have orange dots BELOW the 0 mark, that means you entered Entry Dates into the future, which means there is potentially a mis-keyed date or the user needs technical assistance about how to know what date to enter for the Entry Date. If this is the case, please <a href='mailto:hmis@cohhio.org' target='_blank'>email the HMIS team at hmis@cohhio.org</a></p>.
+        <p>If you have orange dots BELOW the 0 mark, that means you entered Entry Dates into the future, which means there is potentially a mis-keyed date or the user needs technical assistance about how to know what date to enter for the Entry Date. If this is the case, please <a href='mailto:hmis@cohhio.org' target='_blank'>email the HMIS team at hmis@cohhio.org</a>.</p>
       <h4>Is it possible there's a mistake?</h4>
 <p>It's rare that this occurs, but if an Entry Exit has been created, deleted, and then recreated, the Entry Exit's <em>Date Created</em> date is reset, thus inflating the number of days between the Date Created and the Entry Date. If you need us to check if this was the case for a particular dot on the plot, please <a href='mailto:hmis@cohhio.org' target='_blank'>email us</a> and indicate the program and number of days it is displaying that you think may be incorrect so we can verify if this is the issue.</p>"
     ))
