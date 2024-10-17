6c4f0e1083f9ac6ae962d5d2dd50ca55c5ef263e
Trevin Flickinger <twflick@gmail.com>
2024-10-17 15:26:14 -0400

    update renv / packages

--- app.R
+++ app.R
@@ -3,6 +3,7 @@
 # Or use the blue button on top of this file
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+message("Package loaded successfully")
 options( "golem.app.prod" = TRUE)
 RminorElevated::run_app() # add parameters here (if any)
 
