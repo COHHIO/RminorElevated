fa925389433bff561f16c6da152817bea053b2f0
Trevin Flickinger <github.promptly323@passmail.net>
2026-04-15 09:00:06 -0400

    update to prod

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-Sys.setenv(R_CONFIG_ACTIVE = "dev")
+Sys.setenv(R_CONFIG_ACTIVE = "production")
 message("Active config: ", Sys.getenv("R_CONFIG_ACTIVE"))
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
